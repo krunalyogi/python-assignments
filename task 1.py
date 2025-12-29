2f918955f781184f33166c6b178c3cc231b73a10
krunalyogi <krunalyogi9@gmail.com>
2025-12-29 19:32:12 +0530

    Add files via upload

---
+++ task 1.py
@@ -0,0 +1,6 @@
+num1 = int(input("enter a number: "))
+num2 = int(input("enter another number: "))
+print("Addition:",num1+num2)
+print("Subtraction:",num1-num2)
+print("Multiplication:",num1*num2)
+print("Division:",num1/num2)
---
+++ task 2.py
@@ -0,0 +1,3 @@
+firstname=input("Enter your first name: ")
+lastname=input("Enter your last name: ")
+print(f"hello, {firstname} {lastname} welcome to python program")
